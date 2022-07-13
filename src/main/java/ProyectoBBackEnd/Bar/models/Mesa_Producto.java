1a6eb1ee312f786d12d69d61df59e35c9341bd5a
leandrogiova <leandrogiovacchini@gmail.com>
2022-07-13 19:21:34 -0300

    arreglando cositas chiquitas

--- src/main/java/ProyectoBBackEnd/Bar/models/Mesa_Producto.java
+++ src/main/java/ProyectoBBackEnd/Bar/models/Mesa_Producto.java
@@ -8,10 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import org.hibernate.annotations.ManyToAny;
+
 
 
 @Entity
